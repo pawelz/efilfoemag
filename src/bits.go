83bea931c84c9231b713f70775de34a06f6e955f
Paweł Zuzelski <pawelz@execve.pl>
2020-06-13 22:18:24 +0200

    Use 9 bits.
    The neighborhood is 8 bits. But we need 9 bits to include the cell
    itself, which for our purpose is a part of the neighborhood.

--- src/bits.go
+++ src/bits.go
@@ -1,9 +1,9 @@
 package efilfoemag
 
 // Sum returns number of bits set to 1.
-func Sum(c uint8) uint8 {
-	var s uint8
-	for i := 0; i < 8; i++ {
+func Sum(c uint16) uint16 {
+	var s uint16
+	for i := 0; i < 9; i++ {
 		s += c & 1
 		c >>= 1
 	}
