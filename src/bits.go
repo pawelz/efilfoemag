4709ae4e2f5c92bf9f798b34ccd3401a18382d3f
Paweł Zuzelski <pawelz@execve.pl>
2020-06-27 18:48:03 +0200

    Implements Bytes function.
    Bytes parses bit strings like "01101001" to uint8. In go v1.13 we can
    replace it with 0b literals.

--- src/bits.go
+++ src/bits.go
@@ -14,6 +14,8 @@
 
 package bits
 
+import "log"
+
 // Sumbit returns number of bits set to 1.
 func Sum(c uint16) uint16 {
 	var s uint16
@@ -22,4 +24,25 @@ func Sum(c uint16) uint16 {
 		c >>= 1
 	}
 	return s
+}
+
+// Parses string like "01101001" as uint8.
+// TODO(pawelz@execve.pl) replace this with ob-literalz once we're on go v1.13
+//                        https://tip.golang.org/ref/spec#Integer_literals
+func Byte(bs string) uint8 {
+	if l := len(bs); l != 8 {
+		log.Fatalf("expected 8 bits, got %d: %q", l, bs) // srsly
+	}
+	var v uint8
+	for n, b := range bs {
+		switch b {
+		case '1':
+			v |= (uint8(1) << (7 - n))
+		case '0':
+			// do nothing
+		default:
+			log.Fatalf("unexpected bit value %c in %q", b, bs)
+		}
+	}
+	return v
 }
